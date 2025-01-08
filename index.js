7c108249a4f546e74f0486542a8562495abbe983
Treevyy <aarontrevormoore@gmail.com>
2025-01-08 13:36:59 -0600

    added questions and MIT license

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a discription of your project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the usage information",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What are the contribution guidelines?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the test instructions?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your project:",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "None"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address:",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
